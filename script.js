577efc5dbce937636decf760b104f80fcafae985
sam ben <adresse123456789101112@gmail.com>
2023-05-02 12:26:14 +0200

    refresh canvas

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 window.onload = function()
 {
-    var canvas = document.createElement('canvas');
+    var canvas;
+    var ctx;
+    var delay = 1000;
+    var xCoord = 0;
+    var yCoord = 0; 
+    init();
+
+    function init() {
+    canvas = document.createElement('canvas');
     canvas.width = 600;
     canvas.height = 500;
     canvas.style.border = "1px solid";
     // création du canvas
     document.body.appendChild(canvas);
     // canvas accroché sur la page HTML
-
-    var ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
     //retourne un contexte de dessin 
-    ctx.fillStyle = "#ff0000";
-    // attribut fillStyle spécifie la couleur
-    ctx.fillRect(30,30, 100, 50);
-    // fillRect dessine un rectangle plein aux coordonnées (x, y)
-}
+    refreshCanvas();
 
+    }
+
+    function refreshCanvas() {
+        xCoord += 2;
+        yCoord += 2;
+        ctx.clearRect(0,0, canvas.height, canvas.width);
+        ctx.fillStyle = "#ff0000";
+        // attribut fillStyle spécifie la couleur
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+        // fillRect dessine un rectangle plein aux coordonnées (x, y)
+        setTimeout(refreshCanvas, delay);
+    }
+}
