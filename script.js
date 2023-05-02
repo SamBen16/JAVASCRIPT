26dcf2470ead1fb483ecb62fc0f4657d1a49d238
sam ben <adresse123456789101112@gmail.com>
2023-05-02 12:58:43 +0200

    snake creation

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
 window.onload = function()
 {
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var BlockSize = 30;
     var canvas;
     var ctx;
     var delay = 1000;
-    var xCoord = 0;
-    var yCoord = 0; 
+    var snakee;
     init();
 
     function init() {
-    canvas = document.createElement('canvas');
-    canvas.width = 600;
-    canvas.height = 500;
+    var canvas = document.createElement('canvas');
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
     canvas.style.border = "1px solid";
     // création du canvas
     document.body.appendChild(canvas);
     // canvas accroché sur la page HTML
     ctx = canvas.getContext('2d');
     //retourne un contexte de dessin 
+    snakee = new Snake([[6,4], [5, 4], [4,4]]);
     refreshCanvas();
 
     }
 
     function refreshCanvas() {
-        xCoord += 2;
-        yCoord += 2;
-        ctx.clearRect(0,0, canvas.height, canvas.width);
-        ctx.fillStyle = "#ff0000";
-        // attribut fillStyle spécifie la couleur
-        ctx.fillRect(xCoord, yCoord, 100, 50);
-        // fillRect dessine un rectangle plein aux coordonnées (x, y)
+        ctx.clearRect(0,0, canvasHeight, canvasWidth);
+        snakee.draw();
+        snakee.advance();
         setTimeout(refreshCanvas, delay);
+    }
+
+    function drawBlock(ctx, position){
+            var x = position[0] * BlockSize;
+            var y = position[1] * BlockSize;
+            ctx.fillRect(x, y, BlockSize, BlockSize);
+    }
+
+    function Snake(body) {
+        this.body = body;
+        this.draw = function() {
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(var i = 0; i< this.body.length; i++){
+                drawBlock(ctx, this.body[i])
+            }
+            ctx.restore();
+        };
+        this.advance = function() {
+            var nextPosition = this.body[0].slice();
+            nextPosition[0] ++;
+            this.body.unshift(nextPosition);
+            this.body.pop();
+        }
     }
 }
